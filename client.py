a3e24ddadd9eb350ebac82d1db5a427cb4cbd3ef
Gabriela Gonçalves <gabriela.oliveira.goncalves@g.globo>
2022-05-12 20:28:52 -0300

    configura mensagem enviada e recebida

--- client.py
+++ client.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+import json
 import socket
+import time
 
 IP_SERVER = '127.0.0.1'
 PORT = 8080
 DESTINO = (IP_SERVER, PORT)
+
+def current_time():
+    return round(time.time() * 1000)
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # socket.AF_INET = INET (exemplo IPv4)
@@ -11,14 +16,31 @@ tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 tcp.connect(DESTINO) # inicia a conexao TCP
 
+start = current_time()
 while 1:
-  mensagem = input('Mensagem a ser enviada: ') # mensagem recebera dados do teclado
-  tcp.send(bytes(mensagem, "utf8"))
-  # enviar a mensagem para o destino da conexao (IP + porta)
-  # bytes(mensagem,"utf8") = converte tipo str para byte
+  mensagem = input('Mensagem a ser enviada: ').strip() # mensagem recebera dados do teclado
+  data = {}
 
-#recebe do servidor - máx. 1024 bytes
-data = tcp.recv(1024).decode('utf-8')
+  if mensagem.isdigit():
+    data["tipo"] = "int"
+    data["val"] = int(mensagem)
+  elif len(mensagem) == 1:
+    data["tipo"] = "char"
+    data["val"] = mensagem
+  else:
+    data["tipo"] = "string"
+    data["val"] = mensagem
+
+  start = current_time()
+
+  tcp.send(json.dumps(data).encode('utf-8')) # enviar a mensagem para o destino da conexao
+  data = tcp.recv(1024).decode('utf-8')
+
+  end = current_time()
+  print(data)
+  print(f"RTT: {end - start}")
+
+data = tcp.recv(1024).decode('utf-8') #recebe do servidor - máx. 1024 bytes
 print(data)
 
 tcp.close()
--- server.js
+++ server.js
@@ -1,28 +1,46 @@
 const net = require('net');
 
 const PORT = 8080;
+const IP_SERVER = '127.0.0.1';
 
 const handleConnection = socket => {
-	console.log('Alguem se conectou')
+  console.log('Alguem se conectou');
 
-  socket.on('end', () => {
-    console.log('end')
-    socket.write('Desconectado')
+  socket.on('close', () => {
+    console.log('Connection closed');  
+  });
+
+  socket.on('error', (err) => {
+    console.error(`Connection error: ${err.message}`)
   });
 
   socket.on('data', data => {
-    const str = data.toString()
+    if (!data) return;
+    
+    console.log(`Received: ${data} from ${server.address().address}:${PORT}`)
+  
+    const obj = JSON.parse(data);
+    const response = {}
 
-    if(str === 'end') {
-      socket.end()
+    if (obj.tipo === 'int') {
+      response.result = obj.val + 1;
+    } else if (obj.tipo === 'char') {
+      response.result = obj.val == obj.val.toUpperCase() 
+        ? obj.val.toLowerCase()
+        : obj.val.toUpperCase()
     }
+    else if (obj.tipo === 'string') {
+      response.result = [...obj.val].reverse().join("");
+    } else return;
 
-    console.log(str)
+    socket.write(JSON.stringify(response));
     return;
   })
 }
 
 const server = net.createServer(handleConnection);
-server.listen(PORT, '127.0.0.1', () => {
-  console.log(`Server listening for connection requests on socket ${server.address().address}:${PORT}`);
+server.listen(PORT, IP_SERVER, () => {
+  console.log(
+    `Server listening for connection requests on socket ${server.address().address}:${server.address().port}`
+  );
 });
