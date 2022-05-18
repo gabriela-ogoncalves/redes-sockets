8eca2b18934cd1bf79ebfb79b047c35f495973a8
Gabriela Gonçalves <gabriela.oliveira.goncalves@g.globo>
2022-05-18 20:01:17 -0300

    ajusta funçao para calcular o tempo

--- client.py
+++ client.py
@@ -7,18 +7,14 @@ IP_SERVER = input('Digite o IP do servidor: ')
 PORT = 9009
 DESTINO = (IP_SERVER, PORT)
 
-def current_time():
-    return round(time.time() * 1000)
-
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # socket.AF_INET = INET (exemplo IPv4)
 # socket.SOCK_STREAM = usaremos TCP
 
 tcp.connect(DESTINO) # inicia a conexao TCP
 
-start = current_time()
 while 1:
-  mensagem = input('Mensagem a ser enviada: ').strip() # mensagem recebera dados do teclado
+  mensagem = input('\nMensagem a ser enviada: ').strip() # mensagem recebera dados do teclado
   data = {}
 
   if mensagem.isdigit():
@@ -31,16 +27,12 @@ while 1:
     data["tipo"] = "string"
     data["val"] = mensagem
 
-  start = current_time()
+  start = time.time_ns()
 
   tcp.send(json.dumps(data).encode('utf-8')) # enviar a mensagem para o destino da conexao
   data = tcp.recv(1024).decode('utf-8')
-
-  end = current_time()
-  print("Devolvido: ", data)
-  print("RTT: ", end - start)
-
-data = tcp.recv(1024).decode('utf-8') #recebe do servidor - máx. 1024 bytes
-print(data)
+  end = time.time_ns()
+  print("Devolvido:", data)
+  print('RTT: {}ns'.format(end-start))
 
 tcp.close()
